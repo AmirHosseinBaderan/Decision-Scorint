feb8f9ee54f6f22911f3c32218380620bbfa0bbc
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-04-10 02:54:14 +0330

    update : data base

---
+++ DecisionTree/App/Migrations/20250409231947_UpdateMath.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "DecisionNodes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Formula",
+                table: "DecisionNodes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Formula",
+                table: "DecisionNodes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "DecisionNodes",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
